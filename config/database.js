6f2546ccbe106adf64427975f068c647b402f3cf
Guhanandan <guhanandan6542060@gmail.com>
2025-07-30 15:20:59 +0530

    feat: Enhanced the code funtionality

--- config/database.js
+++ config/database.js
@@ -6,12 +6,12 @@ const connectDB = async () => {
     const options = {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      maxPoolSize: 10, // Maintain up to 10 socket connections
-      serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      family: 4, // Use IPv4, skip trying IPv6
-      bufferCommands: false, // Disable mongoose buffering
-      bufferMaxEntries: 0 // Disable mongoose buffering
+      maxPoolSize: 10, 
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000, 
+      family: 4,
+      bufferCommands: false,
+      bufferMaxEntries: 0
     };
 
     const conn = await mongoose.connect(process.env.MONGODB_URI, options);
